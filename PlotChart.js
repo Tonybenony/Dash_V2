3c3dc340d45127ad90dde5d29a59a5500660c5ae
Tonybenony <68955840+Tonybenony@users.noreply.github.com>
2024-12-29 01:38:11 +0100

    Change files

--- PlotChart.js
+++ PlotChart.js
@@ -3,13 +3,12 @@ ipaddress="192.168.178.37"
 port="9990"
 
 i=0;
-
-itemCount = 50;
-watingTime= 100;
+itemCount = 20;
+watingTime= 1000;
 
 var time = [];
-
 var VAL1 = [];
+
 VAL1_Name="Eng Batt";
 VAL1_unit="V";
 
@@ -23,7 +22,13 @@ VAL3_unit="V";
 
 function load(){ScreenWidth();show();};
 function ScreenWidth(){var ScreenWidth = window.screen.width+"px";var ScreenHeight = window.screen.height+"px";document.getElementsByTagName("html")[0].style.width = ScreenWidth;document.getElementsByTagName("html")[0].style.height = ScreenHeight;};
-function show(){const socket = new WebSocket('ws://'+ipaddress+':'+port);socket.addEventListener('open',function (event) {socket.send('Connection Established');});
+
+function show(){
+    const socket = new WebSocket('ws://'+ipaddress+':'+port);
+    socket.addEventListener('open',function (event) {
+        socket.send('Connection Established');
+    });
+
     socket.addEventListener('message',
     function (event) {
         val = event.data;valSort = val.replace(/(\r\n|\n|\r)/gm,"");let numArray = valSort.split(";");
@@ -31,124 +36,60 @@ function show(){const socket = new WebSocket('ws://'+ipaddress+':'+port);socket.
         if (input_lenght < 20){
             console.debug("kleiner 20.\n Eingaben und wird verworfen")
         }else{
-            console.log(numArray.length)
+        count = numArray.length
 
-            item1 =  numArray[0];
-            item2 =  numArray[1];
-            item3 =  numArray[2];
-            item4 =  numArray[3];
-            item5 =  numArray[4];
+        item1 =  numArray[0];
+        item2 =  numArray[1];
+        item3 =  numArray[2];
 
-            vVAL1 = (item1/620)*12;vVAL1 = vVAL1.toFixed(2); 
-            vVAL2 = (item2/620)*12;vVAL2 = vVAL2.toFixed(2);
-            vVAL3 = (item3/620)*12;vVAL3 = vVAL3.toFixed(2);
+        vVAL1 = (item1/620)*12;vVAL1 = vVAL1.toFixed(2); 
+        vVAL2 = (item2/620)*12;vVAL2 = vVAL2.toFixed(2);
+        vVAL3 = (item3/620)*12;vVAL3 = vVAL3.toFixed(2);
 
-            document.getElementsByTagName("graph")[0].getElementsByTagName("value")[0].innerHTML = vVAL1 + " " + VAL1_unit;
-            document.getElementsByTagName("graph")[1].getElementsByTagName("value")[0].innerHTML = vVAL2 + " " + VAL2_unit;
-            document.getElementsByTagName("graph")[2].getElementsByTagName("value")[0].innerHTML = vVAL3 + " " + VAL3_unit;
+        graphText=time;
+        graphVAL1=VAL1;
+        graphVAL2=VAL2;
+        graphVAL3=VAL3;
 
-            document.getElementById("VAL1_Label").innerHTML = VAL1_Name;
-            document.getElementById("VAL2_Label").innerHTML = VAL2_Name;
-            document.getElementById("VAL3_Label").innerHTML = VAL3_Name;
+        VAL1.push(vVAL1);
+        VAL2.push(vVAL2);
+        VAL3.push(vVAL3);
+        time.push("");
 
-            drawCanvas("VAL1" ,vVAL1,10,"#3acb74");
-            drawCanvas("VAL2" ,vVAL2,10,"#3a7ecb");
-            drawCanvas("VAL3" ,vVAL3,10,"#cb3a3a");
-
-            VAL1.push(vVAL1);
-            VAL2.push(vVAL2);
-            VAL3.push(vVAL3);
-
-            const now = new Date();
-            // const year = now.getFullYear();
-            // const day = now.getDay()+3;
-            // const month = now.getMonth() +1;
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
-            const seconds = now.getSeconds();
-
-            time.push(
-                // year + "-" + month + "-" + day + " " + 
-                // hours+
-                // ":"
-                // +minutes+
-                // ":"
-                // +seconds
-                ""
-            );
-
-
-            // if (i > itemCount){
-            //     timelenght = time.length;
-            //     VAL1lenght = VAL1.length;
-            //     VAL2lenght = VAL2.length;
-            //     VAL3lenght = VAL3.length;
-            //     if(timelenght > itemCount){time[""]}
-            //     if(VAL1lenght > itemCount){VAL1[0]}
-            //     if(VAL2lenght > itemCount){VAL2[0]}
-            //     if(VAL3lenght > itemCount){VAL3[0]}
-            // }
-            // console.log(i);
-
-            i++;
-            if (i > itemCount){
-                time.shift();
-                VAL1.shift();
-                VAL2.shift();
-                VAL3.shift();
-            }
-
-            graphText=time;
-            graphVAL1=VAL1;
-            graphVAL2=VAL2;
-            graphVAL3=VAL3;
-
-            lineChart(graphText,graphVAL1,graphVAL2,graphVAL3);
+        if (i > itemCount){
+            time.shift();
+            VAL1.shift();
+            VAL2.shift();
+            VAL3.shift();
         }
-    });
-    function lineChart(){
-    new Chart("myChart", {
+        i++;
+
+        lineChart("myChart", graphText, graphVAL1, '#550055');
+        lineChart("myChart2", graphText, graphVAL2, '#005500');
+        lineChart("myChart3", graphText, graphVAL3, '#555500');
+        
+    };setTimeout(() => load(), watingTime);
+    });}
+
+    function lineChart(target, text, value, color){
+    new Chart(target, {
     type: "line",
     data: {
-        labels: graphText,
+        labels: text,
         datasets: [
         {
-        data: graphVAL1,
-        borderColor: "#c5338b",
-        backgroundColor: "#c5338b53",
+        data: value,
+        borderColor: color,
+        backgroundColor: color+"53",
+        radius: 0,
         label: VAL1_Name,
         fill: true
-        },
-        {
-        data: graphVAL2,
-        borderColor: "#c58133",
-        backgroundColor: "#c5813353",
-        label: VAL2_Name,
-        fill: true
-        },
-        {
-        data: graphVAL3,
-        borderColor: "#33c5c5",
-        backgroundColor: "#33c5c553",
-        label: VAL3_Name,
-        fill: true
         }]
-    },options: {legend: {display: true}, animation: {duration: 0}}});
-    }
-    function drawCanvas(ELEMENT,VALUE,MAXVALUE, COLOR){
-        var endAngle = VALUE/MAXVALUE * Math.PI;
-        var canvas = document.getElementById(ELEMENT);
-        var counterClockwise = false;
-        var context = canvas.getContext('2d');
-        var x = canvas.width / 2;
-        var y = canvas.height / 2;
-        var radius = 65;
-        var startAngle = 0;
-        context.clearRect(0,0,canvas.width,canvas.height);
-        context.beginPath();
-        context.arc(x, y, radius, startAngle, endAngle, counterClockwise)
-        context.lineWidth = 10;
-        context.strokeStyle = COLOR;
-        context.stroke();
-    };setTimeout(() => show(), watingTime);
-}
+    },
+    options: {
+        responsive: true,
+        legend: {display: true}, 
+        animation: {duration: 0},
+        maintainAspectRatio: false
+        
+    }});};
---
+++ PlotValues.js
@@ -0,0 +1,89 @@
+
+ipaddress="192.168.178.37"
+port="9990"
+
+i=0;
+watingTime= 30;
+
+var VAL1 = [];VAL1_Name="Eng Batt";VAL1_unit="V";
+var VAL2 = [];VAL2_Name="Aux Batt";VAL2_unit="V";
+var VAL3 = [];VAL3_Name="Solar";VAL3_unit="V";
+
+var VAL4 = [];VAL4_Name="4";VAL4_unit="V";
+var VAL5 = [];VAL5_Name="5";VAL5_unit="V";
+var VAL6 = [];VAL6_Name="6";VAL6_unit="V";
+
+function load(){ScreenWidth();show();};
+function ScreenWidth(){var ScreenWidth = window.screen.width+"px";var ScreenHeight = window.screen.height+"px";document.getElementsByTagName("html")[0].style.width = ScreenWidth;document.getElementsByTagName("html")[0].style.height = ScreenHeight;};
+function show(){const socket = new WebSocket('ws://'+ipaddress+':'+port);socket.addEventListener('open',function (event) {socket.send('Connection Established');});
+    socket.addEventListener('message',
+    function (event) {
+        val = event.data;valSort = val.replace(/(\r\n|\n|\r)/gm,"");let numArray = valSort.split(";");
+        input_lenght = numArray.length
+        if (input_lenght < 20){
+            console.debug("kleiner 20.\n Eingaben und wird verworfen")
+        }else{
+            count = numArray.length
+
+            item1 =  numArray[0];
+            item2 =  numArray[1];
+            item3 =  numArray[2];
+
+            item4 =  numArray[3];
+            item5 =  numArray[4];
+            item6 =  numArray[5];
+
+            vVAL1 = (item1/620)*12;vVAL1 = vVAL1.toFixed(2); 
+            vVAL2 = (item2/620)*12;vVAL2 = vVAL2.toFixed(2);
+            vVAL3 = (item3/620)*12;vVAL3 = vVAL3.toFixed(2);
+
+            vVAL4 = (item4/620)*12;vVAL4 = vVAL4.toFixed(2);
+            vVAL5 = (item5/620)*12;vVAL5 = vVAL5.toFixed(2);
+            vVAL6 = (item6/620)*12;vVAL6 = vVAL6.toFixed(2); 
+
+            document.getElementsByTagName("graph")[0].getElementsByTagName("value")[0].innerHTML = vVAL1 + " " + VAL1_unit;
+            document.getElementsByTagName("graph")[1].getElementsByTagName("value")[0].innerHTML = vVAL2 + " " + VAL2_unit;
+            document.getElementsByTagName("graph")[2].getElementsByTagName("value")[0].innerHTML = vVAL3 + " " + VAL3_unit;
+
+            document.getElementsByTagName("graph")[3].getElementsByTagName("value")[0].innerHTML = vVAL4 + " " + VAL4_unit;
+            document.getElementsByTagName("graph")[4].getElementsByTagName("value")[0].innerHTML = vVAL5 + " " + VAL5_unit;
+            document.getElementsByTagName("graph")[5].getElementsByTagName("value")[0].innerHTML = vVAL6 + " " + VAL6_unit;
+
+            document.getElementById("VAL1_Label").innerHTML = VAL1_Name;
+            document.getElementById("VAL2_Label").innerHTML = VAL2_Name;
+            document.getElementById("VAL3_Label").innerHTML = VAL3_Name;
+
+            document.getElementById("VAL4_Label").innerHTML = VAL4_Name;
+            document.getElementById("VAL5_Label").innerHTML = VAL5_Name;
+            document.getElementById("VAL6_Label").innerHTML = VAL6_Name;
+
+            drawCanvas("VAL1" ,vVAL1,10,"#3acb74");
+            drawCanvas("VAL2" ,vVAL2,10,"#3a7ecb");
+            drawCanvas("VAL3" ,vVAL3,10,"#cb3a3a");
+
+            drawCanvas("VAL4" ,vVAL4,10,"#cb3a3a");
+            drawCanvas("VAL5" ,vVAL5,10,"#cb3a3a");
+            drawCanvas("VAL6" ,vVAL6,10,"#cb3a3a");
+
+            i++;
+        }
+    });
+    
+function drawCanvas(ELEMENT,VALUE,MAXVALUE, COLOR){
+    var endAngle = VALUE/MAXVALUE * Math.PI;
+    var canvas = document.getElementById(ELEMENT);
+    var counterClockwise = false;
+    var context = canvas.getContext('2d');
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    var radius = 65;
+    var startAngle = 0;
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.beginPath();
+    context.arc(x, y, radius, startAngle, endAngle, counterClockwise)
+    context.lineWidth = 10;
+    context.strokeStyle = COLOR;
+    context.stroke();
+}
+
+};setTimeout(() => load(), watingTime);
